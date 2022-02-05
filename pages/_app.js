178247fa3b506b742b22f60165625a14e3bb6466
eXebyss <mysticlord27@gmail.com>
2022-02-05 16:10:31 +0200

    gtag fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 import '../styles/reset.styles.scss'
 import '../styles/index.scss'
 import '../styles/scrollbar.scss'
@@ -18,7 +20,24 @@ import '../styles/redux-logo.scss'
 import '../styles/about__me.scss'
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Script
+				strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+			/>
+			<Script id='ga-analytics'>
+				{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+			</Script>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
