9bf32bd6cf36b7b81d72ec7d0c3ed9ecc8368743
eXebyss <mysticlord27@gmail.com>
2022-04-03 22:40:40 +0300

    Firebase adapter replaced by MongoDB

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,36 +1,10 @@
 import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
-import { FirebaseAdapter } from '@next-auth/firebase-adapter'
-import { initializeApp, getApp, getApps } from 'firebase/app'
-import {
-	getFirestore,
-	collection,
-	query,
-	getDocs,
-	where,
-	limit,
-	doc,
-	getDoc,
-	addDoc,
-	updateDoc,
-	deleteDoc,
-	runTransaction,
-} from 'firebase/firestore'
-
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-}
-
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const db = getFirestore(app)
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from './lib/mongodb'
 
 export default NextAuth({
+	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		EmailProvider({
 			server: {
@@ -44,19 +18,5 @@ export default NextAuth({
 			from: process.env.EMAIL_FROM,
 		}),
 	],
-	adapter: FirebaseAdapter({
-		db,
-		collection,
-		query,
-		getDocs,
-		where,
-		limit,
-		doc,
-		getDoc,
-		addDoc,
-		updateDoc,
-		deleteDoc,
-		runTransaction,
-	}),
 	secret: process.env.NEXT_PUBLIC_SECRET,
 })
