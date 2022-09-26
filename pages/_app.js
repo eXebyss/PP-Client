7a70c0750758962a9ab63222625087c5672f9ed4
eXebyss <mysticlord27@gmail.com>
2022-09-27 00:41:13 +0300

    newStyles: adding a new page "/messages". Adding an authorization.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import Script from 'next/script'
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/global/index.scss'
 
-function App({ Component, pageProps: { ...pageProps } }) {
+function App({ Component, pageProps: { session, ...pageProps } }) {
 	return (
-		<>
+		<SessionProvider session={session}>
 			<Script
 				strategy='lazyOnload'
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
@@ -19,7 +20,7 @@ function App({ Component, pageProps: { ...pageProps } }) {
           `}
 			</Script>
 			<Component {...pageProps} />
-		</>
+		</SessionProvider>
 	)
 }
 
