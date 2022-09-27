3598a8888d0ba9554bcada41329f596eb215d816
Mihails Fjodorovs <mihails.fjodorovs@magebit.com>
2022-09-27 17:08:55 +0300

    newStyles: updating messages.js

--- pages/apollo-client/messages.js
+++ pages/apollo-client/messages.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import { gql } from '@apollo/client'
-import client from '../../apollo-client'
 import { useSession, signIn, signOut } from 'next-auth/react'
+
+import client from '../../apollo-client'
 import Layout, { siteTitle } from '../../components/Layout/Layout'
 
 export async function getServerSideProps() {
@@ -31,25 +32,33 @@ function Messages({ messages }) {
 	const { data: session } = useSession()
 
 	if (session) {
-		return (
-			<>
-				<span style={{ color: 'yellow' }}>
-					Signed in as: {session.user.email}
-				</span>
-				<br />
-				<button onClick={() => signOut()}>Sign out</button>
-				{messages.map(({ _id, name, email, message, date, dateString }) => (
-					<div key={_id}>
-						<p style={{ color: 'yellow', marginTop: '1rem' }}>
-							📧 {name} ({email}), has written: <br />
-							{message}
-							<br />
-							Date: {dateString ? dateString : date}
-						</p>
-					</div>
-				))}
-			</>
-		)
+		if (session.user.email === process.env.WHITELIST_EMAIL) {
+			return (
+				<>
+					<span>Signed in as: {session.user.email}</span>
+					<br />
+					<button onClick={() => signOut()}>Sign out</button>
+					{messages.map(({ _id, name, email, message, date, dateString }) => (
+						<div key={_id}>
+							<p>
+								📧 {name} ({email}), has written: <br />
+								{message}
+								<br />
+								Date: {dateString ? dateString : date}
+							</p>
+						</div>
+					))}
+				</>
+			)
+		} else {
+			return (
+				<>
+					<h3 className='text-error py-2 fhd:py-4'>Access Denied</h3>
+					<p>It seems, like you are not added to a whitelist.</p>
+					<p>Please, contact system administrator.</p>
+				</>
+			)
+		}
 	}
 
 	return (
@@ -58,9 +67,10 @@ function Messages({ messages }) {
 				<title>{`${siteTitle}: Authorization`}</title>
 				<meta name='description' content='Authorization Page.' />
 			</Head>
-			<span>Not signed in </span>
-			<br />
-			<button onClick={() => signIn()}>Sign in</button>
+			<p className='text-warning font-bold py-2 fhd:py-4'>Not signed in ❗</p>
+			<button className='hover:text-primary' onClick={() => signIn()}>
+				Sign in
+			</button>
 		</Layout>
 	)
 }
@@ -75,8 +85,14 @@ export default function ApolloApp({ messages }) {
 					content='Message list send via contact form on M.F. Portfolio Page.'
 				/>
 			</Head>
-			<h2>📫 Message list:</h2>
-			<Messages messages={messages} />
+			<div className='hero min-h-screen bg-base-200'>
+				<div className='hero-content text-center'>
+					<div className='max-w-md'>
+						<h2 className='justify-center font-bold'>📫 Message list:</h2>
+						<Messages messages={messages} />
+					</div>
+				</div>
+			</div>
 		</Layout>
 	)
 }
