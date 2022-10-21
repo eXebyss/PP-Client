251302d52eaf7b3b2ff83292890d6c2b78c920d9
Mihails Fjodorovs <mihails.fjodorovs@magebit.com>
2022-10-21 22:38:10 +0300

    newStyles: Update TechStack.jsx
    Transition delay and duration are updated for react-simple-icons.

--- components/TechStack/TechStack.jsx
+++ components/TechStack/TechStack.jsx
@@ -32,34 +32,34 @@ import {
 const TechStack = () => {
     return (
         <div className="flex flex-wrap justify-evenly bg-neutral p-4 rounded-md">
-            <Html5 className="text-base-content hover:text-[#E34F26] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <CssThree className="text-base-content hover:text-[#1572B6] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Sass className="text-base-content hover:text-[#CC6699] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Bootstrap className="text-base-content hover:text-[#7952B3] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Tailwindcss className="text-base-content hover:text-[#06B6D4] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Javascript className="text-base-content hover:text-[#F7DF1E] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Typescript className="text-base-content hover:text-[#3178C6] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Nodedotjs className="text-base-content hover:text-[#339933] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Express className="text-base-content hover:text-[#000000] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <ReactJs className="text-base-content hover:text-[#61DAFB] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Redux className="text-base-content hover:text-[#764ABC] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Nextdotjs className="text-base-content hover:text-[#000000] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Alpinedotjs className="text-base-content hover:text-[#8BC0D0] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Graphql className="text-base-content hover:text-[#E10098] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Apollographql className="text-base-content hover:text-[#311C87] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Socketdotio className="text-base-content hover:text-[#010101] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Mongodb className="text-base-content hover:text-[#47A248] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Mysql className="text-base-content hover:text-[#4479A1] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Postgresql className="text-base-content hover:text-[#4169E1] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Webpack className="text-base-content hover:text-[#8DD6F9] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Pwa className="text-base-content hover:text-[#5A0FC8] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Git className="text-base-content hover:text-[#F05032] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Github className="text-base-content hover:text-[#181717] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Docker className="text-base-content hover:text-[#2496ED] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Contentful className="text-base-content hover:text-[#2478CC] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Heroku className="text-base-content hover:text-[#430098] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Microsoftazure className="text-base-content hover:text-[#0078D4] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
-            <Magento className="text-base-content hover:text-[#EE672F] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
+            <Html5 className="text-base-content hover:text-[#E34F26] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <CssThree className="text-base-content hover:text-[#1572B6] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Sass className="text-base-content hover:text-[#CC6699] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Bootstrap className="text-base-content hover:text-[#7952B3] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Tailwindcss className="text-base-content hover:text-[#06B6D4] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Javascript className="text-base-content hover:text-[#F7DF1E] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Typescript className="text-base-content hover:text-[#3178C6] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Nodedotjs className="text-base-content hover:text-[#339933] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Express className="text-base-content hover:text-[#000000] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <ReactJs className="text-base-content hover:text-[#61DAFB] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Redux className="text-base-content hover:text-[#764ABC] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Nextdotjs className="text-base-content hover:text-[#000000] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Alpinedotjs className="text-base-content hover:text-[#8BC0D0] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Graphql className="text-base-content hover:text-[#E10098] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Apollographql className="text-base-content hover:text-[#311C87] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Socketdotio className="text-base-content hover:text-[#010101] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Mongodb className="text-base-content hover:text-[#47A248] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Mysql className="text-base-content hover:text-[#4479A1] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Postgresql className="text-base-content hover:text-[#4169E1] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Webpack className="text-base-content hover:text-[#8DD6F9] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Pwa className="text-base-content hover:text-[#5A0FC8] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Git className="text-base-content hover:text-[#F05032] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Github className="text-base-content hover:text-[#181717] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Docker className="text-base-content hover:text-[#2496ED] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Contentful className="text-base-content hover:text-[#2478CC] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Heroku className="text-base-content hover:text-[#430098] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Microsoftazure className="text-base-content hover:text-[#0078D4] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
+            <Magento className="text-base-content hover:text-[#EE672F] transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-150" />
         </div>
     )
 }
